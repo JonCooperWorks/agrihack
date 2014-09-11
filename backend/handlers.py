8348d62f2cebf7880bb2ea3135661ffc54cb9dca
Jonathan Cooper <jonathan.cooper.2010@gmail.com>
2014-09-11 17:59:33 -0500

    Added support for receiving SMS messages from Digicel numbers

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,7 +1,28 @@
+import logging
+import re
+
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
+
 from backend import BaseHandler
+from models import SMSMessage
 
 
 class MainHandler(BaseHandler):
 
     def get(self):
         self.response.write('Agrihack API')
+
+
+class SMSHandler(InboundMailHandler):
+    """Handles inbound SMS messages from farmers."""
+
+    def receive(self, mail_message):
+        for message in mail_message.bodies('text/html'):
+            _, body = message
+            # Parse phone number from <phone-number>@mms.digicelgroup.com
+            sender, = re.findall(
+                r'([0-9]{11})@mms.digicelgroup.com',
+                mail_message.sender)
+            sms_message = SMSMessage(body=body.decode(), sender=sender)
+            sms_message.put()
+            logging.info('Stored message from %s' % sender)
--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,19 @@ from google.appengine.ext import ndb
 class TestModel(ndb.Model):
 
     field = ndb.StringProperty()
+
+
+class SMSMessage(ndb.Model):
+    body = ndb.StringProperty()
+    sender = ndb.StringProperty()
+
+class Node(ndb.Model):
+    name = ndb.StringProperty()
+
+
+class Crop(ndb.Model):
+    name = ndb.StringProperty()
+    # Ranges
+
+
+
