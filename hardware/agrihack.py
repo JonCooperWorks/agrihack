2413ffbfce784f35da6c2f068e030ce0b197ff91
Orane Edwards <orane.dev@gmail.com>
2014-09-11 23:27:21 -0500

    Added datapoint url.

--- hardware/agrihack.py
+++ hardware/agrihack.py
@@ -2,13 +2,13 @@ import serial, requests, json
 from random import randrange
 
 ser = serial.Serial('/dev/tty.usbserial', 9600)
-agrihack_url = 'http://node-420.appspot.com'
-node_id = 000001
+agrihack_url = 'http://node-420.appspot.com/datapoint/'
+node_id = 00420
 
 while True:
 
   light_data = ser.readline()
-  data = json.dumps('node_id': randrange(10000, 99999),
+  data = json.dumps('node_id': node_id,
                     'temperature': randrang(50, 112),
                     'pressure': randrange(0, 111),
                     'humidity': randrange(0, 100),
