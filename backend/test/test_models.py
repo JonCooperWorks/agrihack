91090d069edd70fc52267892dbde97bbec74483f
Jonathan Cooper <jonathan.cooper.2010@gmail.com>
2014-09-11 22:50:12 -0500

    Added get_datapoints method to Node model and timestamp to DataPoint

--- backend/test/test_models.py
+++ backend/test/test_models.py
@@ -1,3 +1,5 @@
+import datetime
+
 import unittest2
 
 from backend.test import TestCase
@@ -21,6 +23,11 @@ class FarmerTestCase(TestCase, unittest2.TestCase):
         self.assertIsNone(farmer.dob)
         self.assertIsNone(farmer.main_activity)
 
+    def test_get_by_farmer_id(self):
+        farmer_key = Farmer(farmer_id='TEST').put()
+        farmer = Farmer.get_by_farmer_id('TEST')
+        self.assertEqual(farmer_key, farmer.key)
+
 
 class NodeTestCase(TestCase, unittest2.TestCase):
 
@@ -32,13 +39,23 @@ class NodeTestCase(TestCase, unittest2.TestCase):
         node = Node(node_id='TEST').put()
         self.assertEqual(node, Node.get_by_node_id('TEST').key)
 
+    def test_get_datapoints(self):
+        node = Node()
+        node.put()
+        for _ in range(10):
+            DataPoint(parent=node.key).put()
+
+        self.assertEqual(10, len(node.get_datapoints()))
+
 
 class DataPointTestCase(TestCase, unittest2.TestCase):
 
     def test_defaults(self):
         data_point = DataPoint()
+        data_point.put()
         self.assertIsNone(data_point.humidity)
         self.assertIsNone(data_point.light)
         self.assertIsNone(data_point.pressure)
         self.assertIsNone(data_point.saturation)
         self.assertIsNone(data_point.temperature)
+        self.assertTrue(datetime.datetime.now() - data_point.created < datetime.timedelta(minutes=5))
