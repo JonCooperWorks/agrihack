eba21d5fdfa6faf51c2fc1e015dc620c0d3d598c
Orane Edwards <orane.dev@gmail.com>
2014-09-12 12:41:49 -0500

    Updated python communication.

--- hardware/agrihack.py
+++ hardware/agrihack.py
@@ -1,19 +1,21 @@
-import serial, requests, json
+import serial, requests, json, time
 from random import randrange
 
-ser = serial.Serial('/dev/tty.usbserial', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 agrihack_url = 'http://node-420.appspot.com/datapoint/'
-node_id = 00420
+node_id = 'tomatoes'
 
 while True:
 
   light_data = ser.readline()
-  data = json.dumps('node_id': node_id,
-                    'temperature': randrang(50, 112),
-                    'pressure': randrange(0, 111),
-                    'humidity': randrange(0, 100),
-                    'light': light,
-                    'saturation': randrange(0, 100))
+  data = json.dumps({'node_id': str(node_id),
+                    'temperature': str(randrange(50, 112)),
+                    'pressure': str(randrange(0, 111)),
+                    'humidity': str(randrange(0, 100)),
+                    'light': str(light_data),
+                    'saturation': str(randrange(0, 100))})
 
   requests.post(agrihack_url, data)
-  
+  print "data sent"
+  time.sleep(2)
+
